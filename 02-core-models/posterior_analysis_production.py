292c821746342cfdf7a4f8e0d4cd3043368821de
Hening Wang <hening.wang0615@gmail.com>
2024-07-25 13:32:57 +0200

    add test 4

--- 02-core-models/posterior_analysis_production.py
+++ 02-core-models/posterior_analysis_production.py
@@ -13,7 +13,7 @@ from numpyro.infer import MCMC, NUTS
 numpyro.set_platform("cpu")
 import csv
 from numpyro.infer import Predictive
-from modelling_production_data import likelihood_function, import_dataset
+from modelling_production_data import likelihood_function, import_dataset, likelihood_function_map
 
 def read_csv_to_dict(filename):
     data_dict = {}
@@ -44,16 +44,20 @@ def posterior_analysis():
 
     # Access the data dictionary
     for key, values in csv_data.items():
-        print(key, values[:10])
-
-    states_train, empirical_train, df_experiment = import_dataset()
+        print(key)
     
+    states_train, empirical_train, df_experiment = import_dataset()
+    color_semval = csv_data["color_semvalue"]
+    # Compute map of color_semval
+    color_semval_map = jnp.mean(color_semval)
     predictive = Predictive(likelihood_function, csv_data)
     predictions = predictive(rng_key_, states_train, empirical_train)["obs"]
-
+    with numpyro.handlers.seed(rng_seed=0):
+        predictions_map = likelihood_function_map(states_train, 1, color_semval_map, 0.5)
     df_pred = df_experiment
     df_pred["mean_predictions"] = jnp.mean(predictions, axis=0)
     df_pred["std_predictions"] = jnp.std(predictions, axis=0)
+    df_pred["mean_predictions_map"] = predictions_map
     
     df_pred.to_csv('../posterior_samples/production_posteriorPredictive_test2.csv', index=False)
 
