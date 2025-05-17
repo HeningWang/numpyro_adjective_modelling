8482882a4318096848f04368e0580eaaccd8ec80
Hening Wang <hening.wang0615@gmail.com>
2025-05-17 16:32:12 +0200

    set up for another simulation with 3 parameters

--- 02-scripts/run_simulation.sh
+++ 02-scripts/run_simulation.sh
@@ -4,10 +4,10 @@
 export LC_NUMERIC="en_US.UTF-8"
 
 # Define values for arguments
-nobj_list=$(seq 2 4 20)  # Generates a sequence from 2 to 20 with a step of 4, five values in total
+nobj_list=$(seq 2 4 30)  # Generates a sequence from 2 to 30 with a step of 4, 7 values in total
 speaker_list=("incremental_speaker" "global_speaker")  # List of speaker models, two values in total
-color_semvalue_list=$(seq 0.90 0.02 0.99)  # Generates a sequence from 0.90 to 0.99 with a step of 0.02, six values in total
-k_list=(0.5)  # Generates a sequence from 0.2 to 0.8 with a step of 0.2, four values in total
+color_semvalue_list=$(seq 0.9 0.02 0.99)  # Generates a sequence from 0.90 to 0.99 with a step of 0.02, six values in total
+k_list=$(seq 0.1 0.2 0.99)  # Generates a sequence from 0.05 to 0.99 with a step of 0.2, five values in total
 wf_list=(0.5)  # Generates a sequence from 0.2 to 1.0 with a step of 0.2, five values in total
 size_distribution_list=("normal")  # List of size distributions, three values in total
 # Total iterations: 5 (nobj) * 2 (speaker) * 6 (color_semvalue) * 4 (k) * 5 (wf) * 3 (size_distribution) = 1800 * 10000 (sample_size) = 18,000,000
@@ -17,15 +17,15 @@ sample_size=10000
 form_semvalue=0.98
 alpha=1.0
 bias=0.0
+wf =0.5
 world_length=2
+size_distribution="normal"
 
 # Loop over speaker_list, nobj_list, color_semvalue_list, k_list, and wf_list to run the Python script for each combination
 for speaker in "${speaker_list[@]}"; do 
     for nobj in $nobj_list; do
         for color_semvalue in $color_semvalue_list; do
             for k in $k_list; do
-                for wf in $wf_list; do
-                    for size_distribution in "${size_distribution_list[@]}"; do
                         python 01-simulation-random-states.py --nobj $nobj \
                             --sample_size $sample_size \
                             --color_semvalue $color_semvalue \
@@ -37,8 +37,6 @@ for speaker in "${speaker_list[@]}"; do
                             --bias $bias \
                             --world_length $world_length \
                             --size_distribution $size_distribution
-                    done
-                done
             done
         done
     done
